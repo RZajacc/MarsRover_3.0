4dbf0266f1684955f3c712a95e81cabbdbce1936
RZajacc <rf.zajac@gmail.com>
2024-03-31 13:45:04 +0200

    improved a bit a mobile look of the navigation bar, and added an image section below it

--- src/navbar.ts
+++ src/navbar.ts
@@ -1,8 +1,6 @@
 const toggleButton = document.querySelector('.toggle-button')
 const mobileNavItems = document.querySelector('.mobile-nav__items')
-const ddd = document.querySelector('.spirit-desc')
 
 toggleButton?.addEventListener('click', () => {
   mobileNavItems?.classList.toggle('unfold')
-  ddd?.classList.toggle('nav-unfolded')
 })
