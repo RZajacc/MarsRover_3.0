b4f84612abe094a460b1117157f825de195b7573
RZajacc <rf.zajac@gmail.com>
2024-04-03 22:28:44 +0200

    cleaned up image gallery from bootstrap code and restyled it completely

--- src/Utility/displayGallery.ts
+++ src/Utility/displayGallery.ts
@@ -12,69 +12,52 @@ export function displayGallery(
   removeAllChildNodes: (parent: HTMLElement) => void
 ): void {
   // * Get the gallery div and clean it from existing content
-  const photoDiv = document.getElementById('photo-gallery') as HTMLDivElement
-  removeAllChildNodes(photoDiv)
+  const photoGallery = document.getElementById(
+    'photo-gallery'
+  ) as HTMLDivElement
+  removeAllChildNodes(photoGallery)
   const pagesDiv = document.getElementById('pages') as HTMLDivElement
   removeAllChildNodes(pagesDiv)
 
-  // *Create a div containing cards group
-  const cardGroup = document.createElement('div')
-  cardGroup.setAttribute('id', 'galleryCards')
-  cardGroup.setAttribute('class', 'row row-cols-1 row-cols-md-2 g-3')
-  photoDiv.appendChild(cardGroup)
-
-  // *Loop through requested data
+  // Loop through requested data
   data.photos.forEach((element) => {
-    const colCard = document.createElement('div')
-    colCard.setAttribute('class', 'col')
-    cardGroup.appendChild(colCard)
+    // Create single image div
+    const imageDiv = document.createElement('div')
+    imageDiv.setAttribute('class', 'photo-gallery__item')
+    photoGallery.appendChild(imageDiv)
 
-    const cardBody = document.createElement('div')
-    cardBody.setAttribute('class', 'card h-100')
-    colCard.appendChild(cardBody)
-
-    // *Create card body elements
+    // Create anchor tag with image url as link
     const photoRef = document.createElement('a')
     photoRef.setAttribute('href', element.img_src)
+    photoRef.setAttribute('class', 'photo-gallery__item__anchor-tag')
     photoRef.setAttribute('target', '_blank')
-    cardBody.append(photoRef)
-    const cardPhoto = document.createElement('img')
-    cardPhoto.setAttribute('class', 'card-img-top')
-    cardPhoto.setAttribute('src', element.img_src)
-    cardPhoto.setAttribute('alt', 'Made on: ' + element.earth_date)
-    photoRef.appendChild(cardPhoto)
+    imageDiv.append(photoRef)
 
-    const photoDesc = document.createElement('ul')
-    photoDesc.setAttribute('class', 'list-group list-group-flush')
-    cardBody.appendChild(photoDesc)
+    // Create img tag with image link and append it to anchor tag
+    const imageTag = document.createElement('img')
+    imageTag.setAttribute('src', element.img_src)
+    imageTag.setAttribute('alt', 'Made on: ' + element.earth_date)
+    photoRef.appendChild(imageTag)
 
-    const roverLi = document.createElement('li')
-    roverLi.setAttribute('class', 'list-group-item')
-    roverLi.innerHTML = '<strong>Rover : </strong>' + element.rover.name
-    photoDesc.appendChild(roverLi)
-
-    const solLi = document.createElement('li')
-    solLi.setAttribute('class', 'list-group-item')
-    solLi.innerHTML = '<strong>Solar day : </strong>' + element.sol
-    photoDesc.appendChild(solLi)
-
-    const idLi = document.createElement('li')
-    idLi.setAttribute('class', 'list-group-item')
-    idLi.innerHTML = '<strong>Photo ID : </strong>' + element.id
-    photoDesc.appendChild(idLi)
-
-    const camLi = document.createElement('li')
-    camLi.setAttribute('class', 'list-group-item')
-    camLi.innerHTML = '<strong>Camera : </strong>' + element.camera.name
-    photoDesc.appendChild(camLi)
-
-    // *Create a card footer
-    const cardFooter = document.createElement('div')
-    cardFooter.setAttribute('class', 'card-footer')
-    const footerContent = document.createElement('small')
-    footerContent.setAttribute('class', 'text-body-secondary')
-    footerContent.innerHTML = 'Earth date : ' + element.earth_date
-    cardBody.appendChild(cardFooter)
-    cardFooter.appendChild(footerContent)
+    // Create rover span
+    const roverSpan = document.createElement('span')
+    roverSpan.setAttribute('class', 'span__rover')
+    roverSpan.innerText = `${element.rover.name}`
+    imageDiv.appendChild(roverSpan)
+    // Create earth-date span
+    const earthDateSpan = document.createElement('span')
+    earthDateSpan.setAttribute('class', 'span__earth-date')
+    earthDateSpan.innerText = `${element.earth_date}`
+    imageDiv.appendChild(earthDateSpan)
+    // Create imageId span
+    const idSpan = document.createElement('span')
+    idSpan.setAttribute('class', 'span__image-id')
+    idSpan.innerText = `Id - ${element.id}`
+    imageDiv.appendChild(idSpan)
+    // Create camera span
+    const cameraSpan = document.createElement('span')
+    cameraSpan.setAttribute('class', 'span__camera')
+    cameraSpan.innerText = `Cam - ${element.camera.name}`
+    imageDiv.appendChild(cameraSpan)
   })
 }
