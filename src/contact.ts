17886fb522d8986b2f0746af5055e63a49514e7b
RZajacc <rf.zajac@gmail.com>
2024-04-19 12:33:11 +0200

    added a working modal to display result of sending a message in contact form

--- src/contact.ts
+++ src/contact.ts
@@ -7,6 +7,29 @@ import emailjs from '@emailjs/browser'
 })()
 
 const contactForm: HTMLFormElement = document.querySelector('#contact-form')!
+const modal: HTMLDivElement = document.querySelector('.modal')!
+
+const formFeedback = (): void => {
+  if (localStorage.getItem('success') != null) {
+    const successMessage = localStorage.getItem('success')
+    modal.style.display = 'block'
+    const modalBody = document.querySelector('.modal-body')!
+    const modalBodyText = modalBody.firstElementChild as HTMLParagraphElement
+    // console.log(modalBody.firstElementChild)
+    modalBodyText.innerText = successMessage!
+
+    const modalClose = document.querySelector('.modal-close')!
+    const closeButton = modal.firstElementChild as HTMLButtonElement
+    closeButton?.addEventListener('click', () => {
+      modal.style.display = 'none'
+      localStorage.clear()
+    })
+  }
+}
+
+formFeedback()
+// const modal: HTMLDivElement = document.querySelector('.modal')!
+
 // * Send email with data from the document
 contactForm.addEventListener('submit', (event) => {
   // handle the form data
@@ -16,11 +39,16 @@ contactForm.addEventListener('submit', (event) => {
   emailjs
     .sendForm('contact_service', 'contact_form', contactForm)
     .then(() => {
+      localStorage.setItem(
+        'success',
+        "You'll receive a confirmation on your email soon!"
+      )
       contactForm.submit()
     })
     .catch((error) => {
-      const par = document.getElementById('test') as HTMLParagraphElement
-      par.innerText = 'Something went wrong!'
-      console.log('FAILED....', error)
+      localStorage.setItem(
+        'failure',
+        `Ups! Something went wront! ${error.message}"`
+      )
     })
 })
