57ad4d9e1beaeb0f6035e9c2f94592935149fd8b
RZajacc <rf.zajac@gmail.com>
2024-04-19 15:04:53 +0200

    finished styling of modal and logic for displaying depending on the content of localstorage

--- src/contact.ts
+++ src/contact.ts
@@ -6,36 +6,15 @@ import emailjs from '@emailjs/browser'
   emailjs.init('DzZ6BIu3N4pT6e34Q')
 })()
 
+// Query contact form from the page
 const contactForm: HTMLFormElement = document.querySelector('#contact-form')!
-const modal: HTMLDivElement = document.querySelector('.modal')!
 
-const formFeedback = (): void => {
-  if (localStorage.getItem('success') != null) {
-    const successMessage = localStorage.getItem('success')
-    modal.style.display = 'block'
-    const modalBody = document.querySelector('.modal-body')!
-    const modalBodyText = modalBody.firstElementChild as HTMLParagraphElement
-    // console.log(modalBody.firstElementChild)
-    modalBodyText.innerText = successMessage!
-
-    const modalClose = document.querySelector('.modal-close')!
-    const closeButton = modal.firstElementChild as HTMLButtonElement
-    closeButton?.addEventListener('click', () => {
-      modal.style.display = 'none'
-      localStorage.clear()
-    })
-  }
-}
-
-formFeedback()
-// const modal: HTMLDivElement = document.querySelector('.modal')!
-
-// * Send email with data from the document
+// Send email with data from the document
 contactForm.addEventListener('submit', (event) => {
   // handle the form data
   event.preventDefault()
 
-  // *Send form with emailjs
+  // Send form with emailjs and store result in local storage
   emailjs
     .sendForm('contact_service', 'contact_form', contactForm)
     .then(() => {
@@ -48,7 +27,56 @@ contactForm.addEventListener('submit', (event) => {
     .catch((error) => {
       localStorage.setItem(
         'failure',
-        `Ups! Something went wront! ${error.message}"`
+        `Please try again later! ${error.message}"`
       )
     })
 })
+
+// Get result from local storage and display modal depending on the content
+const formFeedback = (): void => {
+  // Query all elements that will be styled depending of the content
+  const modal: HTMLDivElement = document.querySelector('.modal')!
+  const modalHeader: HTMLDivElement = document.querySelector('.modal-header')!
+  const modalBody = document.querySelector('.modal-body')!
+  const modalClose = document.querySelector('.modal-close')!
+  // Check if there is a key stored in the browser
+  if (localStorage.getItem('success') != null) {
+    // Get the message and assign a styling to modal and its content
+    const header = modalHeader.firstElementChild as HTMLHeadingElement
+    header.innerText = 'Message was sent successfully!'
+    const successMessage = localStorage.getItem('success')
+    modal.classList.add('active')
+    modal.firstElementChild?.classList.add('content-success')
+    // Get modal text element and assign a message to modal body text
+    const modalBodyText = modalBody.firstElementChild as HTMLParagraphElement
+    modalBodyText.innerText = successMessage!
+
+    // Get a close modal button, assign event and styling to it
+    const closeButton = modalClose.firstElementChild as HTMLButtonElement
+    closeButton.classList.add('button-success')
+    closeButton?.addEventListener('click', () => {
+      modal.style.display = 'none'
+      localStorage.clear()
+    })
+  } else if (localStorage.getItem('failure') != null) {
+    // Get the message and assign a styling to modal and its content
+    const header = modalHeader.firstElementChild as HTMLHeadingElement
+    header.innerText = 'Something went wrong!'
+    const failureMessage = localStorage.getItem('failure')
+    modal.classList.add('active')
+    modal.firstElementChild?.classList.add('content-failure')
+    // Get modal text element and assign a message to modal body text
+    const modalBodyText = modalBody.firstElementChild as HTMLParagraphElement
+    modalBodyText.innerText = failureMessage!
+
+    // Get a close modal button, assign event and styling to it
+    const closeButton = modalClose.firstElementChild as HTMLButtonElement
+    closeButton.classList.add('button-failure')
+    closeButton?.addEventListener('click', () => {
+      modal.style.display = 'none'
+      localStorage.clear()
+    })
+  }
+}
+
+formFeedback()
