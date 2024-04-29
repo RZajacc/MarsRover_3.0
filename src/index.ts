d8e4bb2a2311410c537bc7c5835beae1b9805003
RZajacc <rf.zajac@gmail.com>
2024-04-29 10:31:15 +0200

    added images to asset manager, and declarations.d.ts file to be able to import images in components

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 import './styles/scss/index.scss'
+
+import roverIcon from '../public/assets/rover_icon.png'
+import spiritImage from '../public/assets/spirit_image.png'
+import opportunityImage from '../public/assets/spirit_image.jpg'
+import curiosityImage from '../public/assets/curiosity_image.png'
+
+document
+  .querySelector('link[rel="shortcut icon"')
+  ?.setAttribute('href', roverIcon as string)
